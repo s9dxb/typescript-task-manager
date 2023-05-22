1adaaaf250ad612b3d46b89bc4210209d43cf47a
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-05-22 16:42:59 +0100

    added App title

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,12 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import React from "react";
 import "./App.css";
 
-function App() {
-  return (
-    <>
-      <div className="App"></div>
-    </>
-  );
-}
+const App: React.FC = () => {
+  return;
+
+  <div className="App">
+    <span className="heading">Taskify</span>
+  </div>;
+};
 
 export default App;
