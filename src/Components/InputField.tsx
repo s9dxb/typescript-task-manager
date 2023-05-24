52c5a8020e72d4c3c40aa2f15aeef2f9a7adc2db
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-05-24 17:01:40 +0100

    added css propreties

--- src/Components/InputField.tsx
+++ src/Components/InputField.tsx
@@ -1,16 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
+import styles from "./styles.css";
 
 const InputField = () => {
+  const [todo, setTodo] = useState("");
+
   return (
     <form className="input">
-      <input
-        type="text"
-        placeholder="Enter a Task"
-        value={todo}
-        ref={inputRef}
-        onChange={(e) => setTodo(e.target.value)}
-        className="input__box"
-      />
+      <input type="text" placeholder="Enter a Task" className="input__box" />
       <button type="submit" className="input_submit">
         GO
       </button>
