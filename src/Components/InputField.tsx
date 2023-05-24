8b4ed4e0e6b6f8e933c4dbb95ed1904026cd9604
Lhasbellaoui Sabrina <sabrina.asbellaoui@gmail.com>
2023-05-24 16:39:00 +0100

    added input field

---
+++ src/Components/InputField.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const InputField = () => {
+  return (
+    <form className="input">
+      <input
+        type="text"
+        placeholder="Enter a Task"
+        value={todo}
+        ref={inputRef}
+        onChange={(e) => setTodo(e.target.value)}
+        className="input__box"
+      />
+      <button type="submit" className="input_submit">
+        GO
+      </button>
+    </form>
+  );
+};
+
+export default InputField;
